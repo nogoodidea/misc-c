6e1f5b817ed2d1a9420088bd7bdf96b1d8b9ac09
nogoodidea <bryce@jauquet.com>
2023-04-04 09:36:56 -0400

    Switching to c++, open gl and glut, sdl2 was not working for me

---
+++ gameTest/OGLTest.cpp
@@ -0,0 +1,47 @@
+// trying /w c++ 
+// https://www.ogldev.org/index.html
+
+// c style stdlib/stdio 
+// it's what i know 
+//TODO CHANGE TO C++ style headers
+#include<cstdio>
+
+#include<GL/freeglut.h>
+
+static void RenderSceneCB(){
+  glClear(GL_COLOR_BUFFER_BIT);
+  glutSwapBuffers();
+}
+
+// glut startup function
+bool intGlut(int* argc,char** argv){
+  // needs argc/argv don't know why
+  glutInit(argc,argv);
+  glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+
+  const int winW = 600;
+  const int winH = 600;
+  glutInitWindowSize(winW,winH);
+  
+  int winX = 400;
+  int winY = 400;
+  glutInitWindowPosition(winX,winY);
+  int win = glutCreateWindow("SUFFERING HERE DOC");
+  if(win == 0){fprintf(stderr,"GLUT ERROR: window failed to init");_Exit(1);}
+  //R,G,B,A
+  glClearColor(0.0f,0.0f,0.0f,0.0f);
+  return true;
+}
+
+// OpenGl startup function
+bool intOGL(){return true;}
+
+
+int main(int argc, char** argv){
+  if(intGlut(&argc,argv) != true){fprintf(stderr,"GLUT INT FAILED");_Exit(1);}
+
+  glutDisplayFunc(RenderSceneCB);
+  
+  glutMainLoop();
+  return 0;
+}
