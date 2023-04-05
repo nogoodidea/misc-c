e4a7fb42d7c989665860eee9789a0086bce3ab80
nogoodidea <bryce@jauquet.com>
2023-04-05 10:22:02 -0400

    now with print buffering

--- gameTest/OGLTest.cpp
+++ gameTest/OGLTest.cpp
@@ -38,6 +38,7 @@ bool intGlut(int* argc,char** argv){
 
 // OpenGl startup function
 bool intOGL(){
+  std::cout << "STARTING SHADER COMP" << std::endl;
   // vertex shader
   // going to be baked in as i don't give an
   //https://learnopengl.com/Getting-started/Hello-Triangle
@@ -55,14 +56,14 @@ bool intOGL(){
   
   // error checking
   GLint error;
-  char GlLog[512];
+  char errorLog[512];
   glGetShaderiv(vertShad,GL_COMPILE_STATUS,&error);
-  
   if(!error){
-    glGetShaderInfoLog(vertShad,512,NULL,GlLog);
-    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << GlLog <<std::endl;
+    glGetShaderInfoLog(vertShad,512,NULL,errorLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << errorLog;
   return false;
   }
+  
   //fragment
   GLuint fragShad = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragShad,1,&fragShadSc,NULL);
@@ -71,27 +72,27 @@ bool intOGL(){
   glGetShaderiv(fragShad,GL_COMPILE_STATUS,&error);
   
   if(!error){
-    glGetShaderInfoLog(fragShad,512,NULL,GlLog);
-    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << GlLog <<std::endl;
+    glGetShaderInfoLog(fragShad,512,NULL,errorLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << errorLog;
   return false;
   } 
-  std::cout << "INFO::SHADER::PROGRAM_COMPILATION_DONE";
+  std::cout << "INFO::SHADER::PROGRAM_COMPILATION_DONE\n";
   return true;
   }
 
 
 int main(int argc, char** argv){
-  std::cout << "INFO::STARTED";
-  if(intGlut(&argc,argv) != true){std::cout << "ERROR::GLUT::INT_FAILED";_Exit(1);}
-  std::cout << "INFO::GLUT::STARTED";
+  if(intGlut(&argc,argv) != true){std::cout << "ERROR::GLUT::INT_FAILED\n";_Exit(1);}
+  std::cout << "INFO::GLUT::STARTED\n";
+  std::cout << std::endl;
   intOGL();
-  
+  std::cout << std::endl; 
   //https://stackoverflow.com/questions/5091570/most-basic-working-vbo-example
   //https://learnopengl.com/Getting-started/Hello-Triangle
   // now with ... PAIN
   // 3 * 3, 3 points 
   
-  int amtPoints = 3;
+  /*int amtPoints = 3;
   GLfloat vertPoints[3*amtPoints]={
   -0.5f,-0.5f,0.0f,
   0.5f,-0.5f,0.0f,
@@ -100,7 +101,7 @@ int main(int argc, char** argv){
   //buffer 
   glGenBuffers(1,&VBO);
   glBindBuffer(GL_ARRAY_BUFFER,VBO);
-  glBufferData(GL_ARRAY_BUFFER,sizeof(GLfloat)*3*amtPoints,vertPoints,GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(GLfloat)*3*amtPoints,vertPoints,GL_STATIC_DRAW);*/
   
   
   glutDisplayFunc(RenderSceneCB);
